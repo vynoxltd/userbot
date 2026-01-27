84a2b255b67fce1c397830aa43175f4068008eac
imnotdetor <avanishpal73092@gmail.com>
2026-01-27 15:29:51 +0530

    Update owner.py

--- plugins/owner.py
+++ plugins/owner.py
@@ -1,6 +1,10 @@
 from pyrogram import filters
 from config import OWNER_ID
 
-owner_only = filters.create(
-    lambda _, __, m: m.from_user and m.from_user.id == OWNER_ID
-)
+def owner_check(_, __, m):
+    return bool(
+        m.from_user and
+        m.from_user.id == OWNER_ID
+    )
+
+owner_only = filters.create(owner_check)

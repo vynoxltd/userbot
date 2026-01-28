9f00c829a7aa50df49b07eb5c8bf9f5aa11cf21b
imnotdetor <avanishpal73092@gmail.com>
2026-01-28 17:14:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-from datetime import datetime
 import os
 
-API_ID = int(os.getenv("API_ID", "0"))
-API_HASH = os.getenv("API_HASH", "")
-OWNER_ID = int(os.getenv("OWNER_ID", "0"))
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+SESSION = os.getenv("SESSION")
+MONGO_URL = os.getenv("MONGO_URL")
 
-START_TIME = datetime.now()
+OWNER_ID = int(os.getenv("OWNER_ID"))

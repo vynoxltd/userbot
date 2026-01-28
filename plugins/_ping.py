3b4202e2a46b65472e0a45bb879918a7dfe9ad56
imnotdetor <avanishpal73092@gmail.com>
2026-01-28 21:02:38 +0530

    Update _ping.py

--- plugins/_ping.py
+++ plugins/_ping.py
@@ -1,6 +1,6 @@
 from telethon import events
 from userbot import bot
 
-@bot.on(events.NewMessage(pattern=r"\.ping$"))
+@bot.on(events.NewMessage(pattern=r"\.pingg"))
 async def ping(e):
     await e.reply("🏓 pong")

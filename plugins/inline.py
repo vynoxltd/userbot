69307e2cf5aa71c4a6fc7a9663881419ad67eeb3
imnotdetor <avanishpal73092@gmail.com>
2026-01-28 18:49:40 +0530

    Delete plugins directory

--- plugins/inline.py
+++
@@ -1,6 +0,0 @@
-from pyrogram import Client, filters
-from plugins.owner import owner_only
-
-@Client.on_message(owner_only & filters.command("help", prefixes="."))
-async def help_cmd(_, m):
-    await m.reply("Help working ✅")

6ea0173ace7845ea1b56d99430855874b13f824d
imnotdetor <avanishpal73092@gmail.com>
2026-01-28 15:31:51 +0530

    Update owner.py

--- plugins/owner.py
+++ plugins/owner.py
@@ -1,17 +1,15 @@
 # plugins/owner.py
 from pyrogram import filters
-import os
-
-OWNER_ID = int(os.getenv("OWNER_ID", "0"))
+from config import OWNER_ID
 
 def owner_check(_, __, m):
     try:
-        # Allow outgoing messages from self
-        if m.from_user and m.from_user.id == OWNER_ID:
+        # outgoing (self)
+        if m.outgoing:
             return True
 
-        # Allow messages sent by you (filters.me equivalent)
-        if m.outgoing:
+        # incoming from owner
+        if m.from_user and m.from_user.id == OWNER_ID:
             return True
 
         return False

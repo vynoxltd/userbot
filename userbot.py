d29f57c5563dc8cae2e867e0e6ac0e2f170263c4
imnotdetor <avanishpal73092@gmail.com>
2026-01-29 07:28:50 +0530

    Update userbot.py

--- userbot.py
+++ userbot.py
@@ -3,12 +3,17 @@ import os
 from telethon import TelegramClient
 from telethon.sessions import StringSession
 
-API_ID = int(os.environ.get("API_ID"))
+API_ID = os.environ.get("API_ID")
 API_HASH = os.environ.get("API_HASH")
 STRING_SESSION = os.environ.get("STRING_SESSION")
 
+if not API_ID or not API_HASH or not STRING_SESSION:
+    raise RuntimeError(
+        "API_ID / API_HASH / STRING_SESSION missing in environment variables"
+    )
+
 bot = TelegramClient(
     StringSession(STRING_SESSION),
-    API_ID,
+    int(API_ID),
     API_HASH
 )
